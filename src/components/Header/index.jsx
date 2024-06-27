8446e0034cf754c9d637f7494dcca8592f22064d
Jessica Bandeira <jessica.santosb@outlook.com>
2024-06-27 15:52:26 -0300

    feat: insert user data on Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,14 @@
+import { useContext, useState } from 'react';
+import { IoIosLogOut, IoMdArrowDropdown } from 'react-icons/io';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { Link, NavLink } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
 import './index.scss';
 
 export default function Header() {
+  const [dropdown, setDropdown] = useState(false);
+  const { userLogout, user } = useContext(UserContext);
+
   const activeLink = {
     color: '#e61e43',
     textDecoration: 'underline',
@@ -15,14 +22,54 @@ export default function Header() {
         BookingRoom.com
       </Link>
 
-      <nav className='header__nav'>
-        <NavLink to={'/login'} className='header__link' style={handleLinkStyle}>
-          Login
-        </NavLink>
-        <NavLink to={'/register'} className='header__link' style={handleLinkStyle}>
-          Register
-        </NavLink>
-      </nav>
+      <button className='header__menu' onClick={() => setDropdown(!dropdown)}>
+        {user ? (
+          <>
+            {user.name}
+            <IoMdArrowDropdown />
+          </>
+        ) : (
+          <RxHamburgerMenu />
+        )}
+      </button>
+
+      <div
+        className={`header__dropdown ${
+          !dropdown && 'header__dropdown--hidden'
+        }`}
+      >
+        {user ? (
+          <>
+            <NavLink
+              to={'/bookings'}
+              className='header__link'
+              style={handleLinkStyle}
+            >
+              Bookings
+            </NavLink>
+            <button onClick={() => userLogout()} className='header__logout'>
+              Logout <IoIosLogOut />
+            </button>
+          </>
+        ) : (
+          <>
+            <NavLink
+              to={'/login'}
+              className='header__link'
+              style={handleLinkStyle}
+            >
+              Login
+            </NavLink>
+            <NavLink
+              to={'/register'}
+              className='header__link'
+              style={handleLinkStyle}
+            >
+              Register
+            </NavLink>
+          </>
+        )}
+      </div>
     </header>
   );
 }
