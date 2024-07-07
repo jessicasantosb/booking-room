393ec7c6181003abc74c6c545944571f40111f98
Jessica Bandeira <jessica.santosb@outlook.com>
2024-07-07 18:52:18 -0300

    feat: add user data to profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,22 @@
+import { useContext } from 'react'
 import './index.scss'
+import { UserContext } from '../../contexts/UserContext'
 
 export default function Profile() {
+  const {user} = useContext(UserContext)
+
   return (
-    <div>
-      profile      
+    <div className='profile'>
+      <p>
+        Nome: <span>{user.name}</span>
+      </p>
+      <p>
+        Email: <span>{user.email}</span>
+      </p>
+      <p>
+        Administrador: <span>{user.isAdmin ? 'Sim' : 'Não'}</span>
+      </p>
+      <button className='profile__button'>Liberar acesso como Administrador</button>
     </div>
-  )
+  );
 }
