afa3b28658568706d30744b17e00256525abd416
Jessica Bandeira <jessica.santosb@outlook.com>
2024-07-09 11:03:45 -0300

    feat: display all rooms

--- src/contexts/AdminContext.jsx
+++ src/contexts/AdminContext.jsx
@@ -5,6 +5,7 @@ export const AdminContext = createContext();
 
 export function AdminProvider({ children }) {
   const [allBookings, setAllBookings] = useState([]);
+  const [allRooms, setAllRooms] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -20,9 +21,28 @@ export function AdminProvider({ children }) {
     }
   };
 
+  const getAllRooms = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('/api/admin/getallrooms');
+      setAllRooms(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  };
+
   return (
     <AdminContext.Provider
-      value={{getAllBookings, allBookings, error, loading}}
+      value={{
+        getAllBookings,
+        getAllRooms,
+        allBookings,
+        allRooms,
+        error,
+        loading,
+      }}
     >
       {children}
     </AdminContext.Provider>
--- src/pages/Admin/AllRooms/index.jsx
+++ src/pages/Admin/AllRooms/index.jsx
@@ -1,5 +1,51 @@
+import { useContext, useEffect } from 'react';
+import Error from '../../../components/interfaces/Error';
+import Loading from '../../../components/interfaces/Loading';
+import { AdminContext } from '../../../contexts/AdminContext';
 import './index.scss';
 
 export default function AllRooms() {
-  return <section>all rooms</section>;
+  const { getAllRooms, allRooms, error, loading } = useContext(AdminContext);
+
+  useEffect(() => {
+    getAllRooms();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <section className='allrooms'>
+      {error && <Error error='' />}
+      <h1 className='allrooms__title'>Todos os quartos disponíveis</h1>
+      <div className='allrooms__table'>
+        <div className='allrooms__table--header'>
+          <p>ID da Quarto</p>
+          <p>Nome</p>
+          <p>Tipo</p>
+          <p>Valor por dia</p>
+          <p>Máx. de pessoas</p>
+          <p>Contato</p>
+        </div>
+        {allRooms.map(
+          ({ _id, name, type, rentproperty, maxcount, phonenumber }) => {
+            return (
+              <div key={_id} className='allrooms__table--content'>
+                <p>{_id}</p>
+                <p>{name}</p>
+                <p>{type}</p>
+                <p>
+                  {rentproperty.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })}
+                </p>
+                <p>{maxcount}</p>
+                <p>{phonenumber}</p>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
 }
