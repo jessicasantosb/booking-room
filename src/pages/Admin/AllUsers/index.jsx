f0353f5ddd711379eb94c12ee9aa7516cd9166e4
Jessica Bandeira <jessica.santosb@outlook.com>
2024-08-06 15:47:19 -0300

    feat: display all users createdAt date

--- src/pages/Admin/AllUsers/index.jsx
+++ src/pages/Admin/AllUsers/index.jsx
@@ -1,6 +1,4 @@
 import { useContext, useEffect } from 'react';
-// import TableInput from '../../../components/Admin/TableInput';
-// import TableSelect from '../../../components/Admin/TableSelect';
 import Error from '../../../components/interfaces/Error';
 import Loading from '../../../components/interfaces/Loading';
 import { AdminContext } from '../../../contexts/AdminContext';
@@ -26,34 +24,19 @@ export default function AllUsers() {
         options={['administrador', 'usuário']}
       />
 
-      {/* 
-      <TableFilter
-        id='users'
-        label='Pesquise pelo ID do usuário, nome ou email'
-        searchResults={duplicateUsers}
-        setSearchResults={setAllUsers}
-      />
-
-      <TableSelect
-        id='users'
-        label='Administrador:'
-        options={['sim', 'não']}
-        array={duplicateUsers}
-        getAll={getAllUsers}
-        setAll={setAllUsers}
-      /> */}
-
       {error && <Error error='Erro ao carregar os dados.' />}
       <div className='table'>
         <div className='table__header allusers__tableHeader'>
+          <p>Data de Criação</p>
           <p>ID do Usuário</p>
           <p>Nome</p>
           <p>Email</p>
           <p>Administrador</p>
         </div>
-        {allUsers.map(({ _id, name, email, isAdmin }) => {
+        {allUsers.map(({ _id, createdAt, name, email, isAdmin }) => {
           return (
             <div key={_id} className='table__content allusers__tableContent'>
+              <p>{new Date(createdAt).toLocaleDateString('pt-BR')}</p>
               <p>{_id}</p>
               <p>{name}</p>
               <p>{email}</p>
