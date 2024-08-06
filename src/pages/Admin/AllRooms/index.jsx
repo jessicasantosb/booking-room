678128521723319c3ecec19c89d645404cdd13cb
Jessica Bandeira <jessica.santosb@outlook.com>
2024-08-06 15:43:11 -0300

    feat: display all rooms date

--- src/pages/Admin/AllRooms/index.jsx
+++ src/pages/Admin/AllRooms/index.jsx
@@ -12,6 +12,9 @@ export default function AllRooms() {
     getAllRooms();
   }, []);
 
+  console.log(allRooms);
+  
+
   if (loading) return <Loading />;
 
   return (
@@ -27,17 +30,27 @@ export default function AllRooms() {
       {error && <Error error='Erro ao carregar os dados.' />}
       <div className='table'>
         <div className='table__header allrooms__tableHeader'>
+          <p>Data de Criação</p>
           <p>ID da Quarto</p>
-          <p>Nome do quarto</p>
+          <p>Nome do Quarto</p>
           <p>Tipo</p>
           <p>Diária</p>
-          <p>Máx. de pessoas</p>
+          <p>Máx. de Pessoas</p>
           <p>Contato</p>
         </div>
         {allRooms.map(
-          ({ _id, name, type, rentproperty, maxcount, phonenumber }) => {
+          ({
+            _id,
+            createdAt,
+            name,
+            type,
+            rentproperty,
+            maxcount,
+            phonenumber,
+          }) => {
             return (
               <div key={_id} className='table__content allrooms__tableContent'>
+                <p>{new Date(createdAt).toLocaleDateString('pt-BR')}</p>
                 <p>{_id}</p>
                 <p>{name}</p>
                 <p>{type}</p>
