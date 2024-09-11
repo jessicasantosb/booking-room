4af57767368e72e8feeaff0283e3fd377bfb9b72
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-11 16:37:03 -0300

    fix: update api url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://booking-room-backend.vercel.app',
+        target: 'https://booking-room-api-99ca.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
